d853a8503f40284de243575a6508dba65768c504
jozw <jozwright@gmail.com>
2014-10-23 15:01:15 -0700

    retrieve cluster info

--- spec/cluster_spec.rb
+++ spec/cluster_spec.rb
@@ -17,5 +17,23 @@ describe 'Tutum' do
         expect_return
       end
     end
+
+    it "can retrieve a cluster's information" do
+
+      UUID = '7a1138f6-75fd-499d-8029-ff407eec4c61'
+      service_instance = service_instance('tutum_cluster')
+
+      params = {
+        'username' => @username,
+        'api_key' => @api_key,
+        'UUID' => UUID
+      }
+
+      service_instance.test_action('retrieve', params) do
+        expect_info message: 'Initializing connection to Tutum'
+        expect_info message: 'Parsing cluster response'
+        expect_return
+      end
+    end
   end
 end
