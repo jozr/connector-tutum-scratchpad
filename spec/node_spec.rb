96605d50070825949a63d12438a066c3adc6fa48
jozw <jozwright@gmail.com>
2014-10-23 15:12:08 -0700

    retrieve node information

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -17,5 +17,24 @@ describe 'Tutum' do
         expect_return
       end
     end
+
+    it "can retrieve a node's information" do
+
+      UUID = '3f5f2bf1-704a-4ba1-921d-43be01bc0b55'
+
+      service_instance = service_instance('tutum_node')
+
+      params = {
+        'username' => @username,
+        'api_key' => @api_key,
+        'UUID' => UUID
+      }
+
+      service_instance.test_action('retrieve', params) do
+        expect_info message: 'Initializing connection to Tutum'
+        expect_info message: 'Parsing node information'
+        expect_return
+      end
+    end
   end
 end
