2826176312465749ac7bf6063b7cc84315df6206
jozwright <jozwright@gmail.com>
2014-11-06 20:46:09 -0800

    create [service] before tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,24 +13,40 @@ c.include Factor::Connector::Test
   c.before do
     @username     = ENV['TUTUM_USERNAME']
     @api_key      = ENV['TUTUM_API_KEY']
-    @service_id   = ENV['TUTUM_SERVICE_ID']
+    @image_url    = ENV['TUTUM_IMAGE_URL'] 
+    @container_id = ENV['TUTUM_CONTAINER_ID']  
     @session      = Tutum.new(@username, @api_key)
-    params = {
+
+    cluster_params = {
       name: 'TEST49',
       node_type: '/api/v1/nodetype/digitalocean/512mb/',
       region: '/api/v1/region/digitalocean/lon1/',
       target_num_nodes: 1
     }
-    cluster       = @session.node_clusters.create(params)
+
+    service_params = {
+      image: @image_url,
+      name: 'TEST48',
+      target_num_containers: 1
+    }
+
+    cluster       = @session.node_clusters.create(cluster_params)
+    service       = @session.services.create(service_params)
+
     @cluster_id   = cluster['uuid']
+    @service_id   = service['uuid']
     @node_id      = cluster['nodes'].first.split('/').last
-    service       = @session.services.get(@service_id)
-    @container_id = service['containers'].first.split('/').last
   end
 
   c.after do
-    response = @session.node_clusters.list
-    response['objects'].each do |cluster|
+    services_response = @session.services.list
+    services_response['objects'].each do |service|
+      if service['name'] == 'TEST48' && service['state'] != 'Terminated'
+        @session.services.terminate(service['uuid'])
+      end
+    end
+    clusters_response = @session.node_clusters.list
+    clusters_response['objects'].each do |cluster|
       if cluster['name'] == 'TEST49' && cluster['state'] != 'Terminated'
         @session.node_clusters.terminate(cluster['uuid'])
       end
